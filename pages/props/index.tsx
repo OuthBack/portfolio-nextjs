1a173ceacd654b934ef2d84f0d37dfe86db76713
OuthBack <riquessan@gmail.com>
2021-01-28 21:41:47 -0300

    GetStaticPaths

--- pages/props/index.tsx
+++ pages/props/index.tsx
@@ -1,18 +1,40 @@
-import { GetStaticPropos } from 'next'
+import { GetStaticPropos, GetStaticPaths } from 'next'
 import path from 'path'
 
-// Executed on server
+/**
+ * Executed on server
+ * 
+ * Only will works if are in
+ * a static page
+ */
 export const getStaticProps: GetStaticPropos = async (context) => {
 
 
-    const fs = require('fs')
+    const fs = require('fs') // fs = filesystem
     const txt = fs.readFileSync(
         path.join(process.cwd(), 'public/robot.txt'), 'utf8')
 
     return {
+        revalidate: 10,
+        /**
+         * Revalidate only work ON PRODUCTION
+         * He will update every 10 seconds
+         * to renew the content
+         * 
+         * ON PRODUCTION don't matter if you
+         * trying to refresh the page, only
+         * will work if the time(10 secs) over
+         */
         props: {
             myFavNum: txt
         }
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async () =>{
+    return{
+        paths: [],
+        fallback: false
     }
 }
 
