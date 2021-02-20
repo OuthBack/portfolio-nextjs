65597cbcdb76dfbb0478bf3848150c9afa02457a
OuthBack <riquessan@gmail.com>
2021-02-19 21:00:02 -0300

    fix cinquini mobile image

--- pages/cinquini.tsx
+++ pages/cinquini.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import {
     BrowserView,
     MobileView,
+    isBrowser,
+    isMobile
   } from "react-device-detect";
 
 
@@ -18,14 +20,14 @@ export default function Cinquini(){
                 />
             </BrowserView>
             <MobileView>
-                <div className="mobile-cinquini">
+                <main className="mobile-cinquini">
                     <Image 
                         src="/images/cinquini-mobile.png" 
                         alt="The Cinquini's" 
                         width={270}
                         height={10476}
                     />
-                </div>
+                </main>
             </MobileView>
         </>
     )
